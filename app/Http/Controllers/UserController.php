67ce7ab068b1afece7d1ba11d11eaf61725e7885
zeinak112 <zeinak112@example.com>
2026-03-26 01:01:18 +0200

    Add UserController for profile updates

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -6,7 +6,7 @@ use Illuminate\Http\Request;
 
 class UserController extends Controller
 {
-    // تحديث النوع
+    
     public function updateGender(Request $request)
     {
         $request->validate([
